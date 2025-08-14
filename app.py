6663b1765d860d9d38d1982e00e04e8f05432759
dranoto <137184559+dranoto@users.noreply.github.com>
2025-08-14 10:53:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 import json
 import fitz # PyMuPDF
-import google.api_core.exceptions
 from google import genai
 import wave
 
